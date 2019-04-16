95d1bf5f4321ca6262645a3dc987c574c6af705f
risanghapsari <risang@mail.ugm.ac.id>
2019-04-16 19:06:17 +0700

    Updates

---
+++ precache-manifest.381c13e9ffbc6f5c20d768b0308b7573.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2ceef7fcbd35532b1b5",
+    "url": "/react/static/js/runtime~main.b2ceef7f.js"
+  },
+  {
+    "revision": "96c571ed345518ebcf28",
+    "url": "/react/static/js/main.96c571ed.chunk.js"
+  },
+  {
+    "revision": "5abe0ad17a6240b5c5d2",
+    "url": "/react/static/js/2.5abe0ad1.chunk.js"
+  },
+  {
+    "revision": "96c571ed345518ebcf28",
+    "url": "/react/static/css/main.eef7e3a2.chunk.css"
+  },
+  {
+    "revision": "a64efa5beebb499d7a59ae0a4607f5bf",
+    "url": "/react/index.html"
+  }
+];
